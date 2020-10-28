c9c6a6b54aef1975ee51b9abcf281ef496f6ddfe
Mohammad Zawad Tahmeed <67639924+mz-tahmeed@users.noreply.github.com>
2020-10-29 04:24:19 +0600

    Basic Game before AI

--- flappybirdai.py
+++ flappybirdai.py
@@ -3,9 +3,10 @@ import neat
 import time
 import os
 import random
+pygame.font.init()
 
 WIN_WIDTH = 500
-WIN_HEIGHT = 600
+WIN_HEIGHT = 800
 
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
@@ -13,6 +14,7 @@ PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 class Bird:
 	IMGS = BIRD_IMGS
@@ -91,17 +93,17 @@ class Pipe:
 	def __init__(self, x):
 		self.x = x
 		self.height = 0
-		self.gap = 100
+		
 
 		self.top = 0
 		self.bottom = 0
 		self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
 		self.PIPE_BOTTOM = PIPE_IMG
 
-		self.passed =False
-		self.height()
+		self.passed = False
+		self.set_height()
 
-	def self_height(self):
+	def set_height(self):
 		self_height = random.randrange(50, 450)
 		self.top = self.height - self.PIPE_TOP.get_height()
 		self.bottom = self.height + self.GAP
@@ -113,7 +115,7 @@ class Pipe:
 		win.blit(self.PIPE_TOP, (self.x, self.top))
 		win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
 
-	def collide(self):
+	def collide(self, bird):
 		bird_mask = bird.get_mask()
 		top_mask = pygame.mask.from_surface(self.PIPE_TOP)
 		bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
@@ -122,7 +124,7 @@ class Pipe:
 		bottom_offset = (self.x - bird.x, self.bottom -round(bird.y))
 
 		t_point = bird_mask.overlap(top_mask, top_offset)
-		b_point = bird_mask.overlap(bottom_mask, top_offset)
+		b_point = bird_mask.overlap(bottom_mask, bottom_offset)
 
 		if t_point or b_point:
 			return True
@@ -136,7 +138,6 @@ class Base:
 	IMG = BASE_IMG
 
 	def __init__(self, y):
-
 		self.y = y
 		self.x1 = 0
 		self.x2 = self.WIDTH
@@ -155,17 +156,29 @@ class Base:
 		win.blit(self.IMG, (self.x1, self.y))
 		win.blit(self.IMG, (self.x2, self.y))
 
-
-def draw_window(win, bird):
+#To show component on the screen
+def draw_window(win, bird, pipes, base, score):
 	win.blit(BG_IMG, (0,0))
+
+	for pipe in pipes:
+		pipe.draw(win)
+
+	text = STAT_FONT.render("Score: " + str(score), 1,(255,255,255))
+	win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+	base.draw(win)
+
 	bird.draw(win)
 	pygame.display.update()
 
-def main():
-	bird = Bird(200, 300)
+def main(genome, config):
+	bird = Bird(230, 350)
+	base = Base(730)
+	pipes = [Pipe(600)]
 	win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 	clock = pygame.time.Clock()
 
+	score = 0
 
 	run = True
 	while run:
@@ -174,8 +187,36 @@ def main():
 			if event.type == pygame.QUIT:
 				run = False
 
-		bird.move()
-		draw_window(win, bird)
+			
+		#bird.move()
+		add_pipe =False
+		rem = []
+		for pipe in pipes:
+			if pipe.collide(bird):
+				pass
+
+			if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+				rem.append(pipe)
+
+			if not pipe.passed and pipe.x < bird.x:
+				pipe.passed = True
+				add_pipe = True
+
+			pipe.move()
+
+		if add_pipe:
+			score += 1
+			pipes.append(Pipe(600))
+
+		for r in rem:
+			pipes.remove(r)
+
+		if bird.y + bird.img.get_height() >= 730:
+			pass
+
+
+		base.move()
+		draw_window(win, bird, pipes, base, score)
 
 
 	pygame.quit()
@@ -183,6 +224,39 @@ def main():
 
 main()
 
+def run(config_file):
+    """
+    runs the NEAT algorithm to train a neural network to play flappy bird.
+    :param config_file: location of config file
+    :return: None
+    """
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    #p.add_reporter(neat.Checkpointer(5))
+
+    # Run for up to 50 generations.
+    winner = p.run(eval_genomes, 50)
+
+    # show final stats
+    print('\nBest genome:\n{!s}'.format(winner))
+
+
+if __name__ == '__main__':
+    # Determine path to configuration file. This path manipulation is
+    # here so that the script will run successfully regardless of the
+    # current working directory.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    run(config_path)
 
 	
 
